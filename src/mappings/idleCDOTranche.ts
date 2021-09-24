0ac208072b55b0722db89cb3e438c41c5f525b4e
Asaf <asafsilman@gmail.com>
2021-09-24 22:00:17 +0800

    Correctly process tranche deposits and withdrawals

--- src/mappings/idleCDOTranche.ts
+++ src/mappings/idleCDOTranche.ts
@@ -1,7 +1,9 @@
 import { BigInt } from '@graphprotocol/graph-ts'
 import { Address } from "@graphprotocol/graph-ts";
-import { Tranche, depositAAEvent, depositBBEvent, withdrawAAEvent, withdrawBBEvent } from "../../generated/schema";
+import { Tranche, depositAAEvent, depositBBEvent, withdrawAAEvent, withdrawBBEvent, transferAA, transferBB } from "../../generated/schema";
 import { Transfer } from "../../generated/templates/IdleCDOTranche/IdleCDOTranche"
+
+import { ADDRESS_ZERO } from './helpers';
 
 export function handleTransfer(event: Transfer): void {
     let fromAddress = event.params.from;
@@ -10,7 +12,7 @@ export function handleTransfer(event: Transfer): void {
     let tranche = Tranche.load(event.address.toHexString());
     let eventId = event.transaction.hash.toHexString() + "_" + event.logIndex.toHexString()
 
-    if (fromAddress == Address.fromI32(0)) {
+    if (fromAddress.toHexString() == ADDRESS_ZERO) {
         // if the from address is 0x0, then the event is a deposit
         tranche.totalSupply = tranche.totalSupply.plus(event.params.value);
         if (tranche.type == "AATranche") {
@@ -26,7 +28,7 @@ export function handleTransfer(event: Transfer): void {
             depositBB.tranche = tranche.id;
             depositBB.save()
         }
-    } else if (toAddress == Address.fromI32(0)) {
+    } else if (toAddress.toHexString() == ADDRESS_ZERO) {
         // if the to address is 0x0m then the event is a withdraw
         tranche.totalSupply = tranche.totalSupply.minus(event.params.value);
         if (tranche.type == "AATranche") {
@@ -44,6 +46,26 @@ export function handleTransfer(event: Transfer): void {
         }
     } else {
         // otherwise this is a normal tranfer
+        if (tranche.type == "AATranche") {
+            let tranferAA = new transferAA(eventId)
+            tranferAA.sender = event.transaction.from;
+            tranferAA.amount = event.params.value;
+            tranferAA.tranche = tranche.id;
+
+            tranferAA.from = event.params.from;
+            tranferAA.to = event.params.to;
+
+            tranferAA.save()
+        } else {
+            let tranferBB = new transferBB(eventId)
+            tranferBB.sender = event.transaction.from;
+            tranferBB.amount = event.params.value;
+            tranferBB.tranche = tranche.id;
+
+            tranferBB.from = event.params.from;
+            tranferBB.to = event.params.to;
+            tranferBB.save()
+        }
     }
 
     tranche.save();
